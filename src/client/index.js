88905560387c106f8f74134097bca895598e2a61
Thomas <tyger.x@zoho.eu>
2021-01-28 16:22:45 +0100

    add back the service workers

--- src/client/index.js
+++ src/client/index.js
@@ -5,14 +5,14 @@ import { handleSubmit } from './js/formHandler';
 import './styles/main.scss';
 
 // Service Worker 
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', () => {
-//         navigator.serviceWorker.register('/service-worker.js').then(registration => {
-//             console.log('SW registered: ', registration);
-//         }).catch(registrationError => {
-//             console.log('SW registration failed: ', registrationError);
-//         });
-//     });
-// }
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+            console.log('SW registered: ', registration);
+        }).catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+        });
+    });
+}
 
 export { checkInput, handleSubmit };
